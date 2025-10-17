818f94d4bd40887c2752a242d7efb562b27bcabb
Marcelo Matos <marcelofmatos@gmail.com>
2025-10-17 10:37:24 -0400

    fix: corrigir uso da variável APP_USER no script app-scheduler.sh

--- base/app-scheduler.sh
+++ base/app-scheduler.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
-_term() {
-    echo 'Terminating scheduler'
-    su -c "otrs.Daemon.pl stop" otrs 
-}
-
-trap _term SIGINT SIGTERM
 
 export START_SCHEDULER=${START_SCHEDULER:-1}
 export DEBUG_MODE=${DEBUG_MODE:-0}
 export APP_USER=${APP_USER:-otrs}
 export SMTPSERVER=${SMTPSERVER:-mail}
 export SMTPPORT=${SMTPPORT:-25}
-export EMAIL=${EMAIL:-"otrs@localhost"}
+export EMAIL=${EMAIL:-"$APP_USER@localhost"}
 export EMAILPASSWORD=${EMAILPASSWORD:-"passw0rd"}
 
-su -c "otrs.Daemon.pl start" otrs 
-Cron.sh start otrs 
+_term() {
+    echo 'Terminating scheduler'
+    su -c "otrs.Daemon.pl stop" $APP_USER 
+}
+
+trap _term SIGINT SIGTERM
+
+su -c "otrs.Daemon.pl start" $APP_USER 
+Cron.sh start $APP_USER  
 /usr/sbin/cron -f -L 15 &
 wait "$!"
