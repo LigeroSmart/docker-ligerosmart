6f7642aa906f93671e28e6c12a2432759ac1fb89
Marcelo Matos <marcelofmatos@gmail.com>
2025-12-12 18:30:17 -0400

    feat: ativar execução do strace no script ligero-starman-strace.sh

--- nginx-dev/usr/local/bin/ligero-starman-strace.sh
+++ nginx-dev/usr/local/bin/ligero-starman-strace.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# strace -e trace=file starman --workers=1 --listen=:9000 /app.psgi 2>&1 | grep -v ENOENT
+strace -e trace=file starman --workers=1 --listen=:9000 /app.psgi 2>&1 | grep -v ENOENT
 
-strace -e trace=file -qq starman --workers=1 --listen=:9000 \
-  -M Plack::Loader \
-  -M Plack::Middleware::AccessLog \
-  -M Starman::Server \
-  -M Net::Server::PreFork \
-  -M HTTP::Parser::XS \
-  -M Devel::StackTrace \
-  /app.psgi 2>&1 | grep -v ENOENT
+# strace -e trace=file -qq starman --workers=1 --listen=:9000 \
+#   -M Plack::Loader \
+#   -M Plack::Middleware::AccessLog \
+#   -M Starman::Server \
+#   -M Net::Server::PreFork \
+#   -M HTTP::Parser::XS \
+#   -M Devel::StackTrace \
+#   /app.psgi 2>&1 | grep -v ENOENT
